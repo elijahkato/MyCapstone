cdece408baf8841b828339f729bc2ea73bce1d13
Inetanbor Osatufo Elijah <elijahosas@gmail.com>
2024-10-06 01:11:50 +0100

    Streamlining Folder

--- MyCapstone/core/urls.py
+++
@@ -1,9 +0,0 @@
-# core/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin interface
-    path('api/', include('inventory_manager.urls')),  # Include your app's URLs at the "api/" path
-]
